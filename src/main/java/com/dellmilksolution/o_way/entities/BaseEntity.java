7b115707f85254c1786812d4ab4aa6f707fba29c
cvnxsolution <cvnxsolution@gmail.com>
2025-11-30 07:43:01 +0630

    feat(basic wallet): add initial basic wallet model

--- src/main/java/com/dellmilksolution/o_way/entities/BaseEntity.java
+++ src/main/java/com/dellmilksolution/o_way/entities/BaseEntity.java
@@ -1,4 +1,42 @@
 package com.dellmilksolution.o_way.entities;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", insertable = false)
+    private LocalDateTime createdAt;
+
+    @CreatedDate
+    @Column(name = "created_by", updatable = false)
+    private Long createdBy;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", insertable = false)
+    private LocalDateTime updatedAt;
+
+    @LastModifiedBy
+    @Column(name = "updated_by", updatable = false)
+    private Long updatedBy;
+
 }
--- src/main/java/com/dellmilksolution/o_way/entities/Wallet.java
+++ src/main/java/com/dellmilksolution/o_way/entities/Wallet.java
@@ -1,4 +1,27 @@
 package com.dellmilksolution.o_way.entities;
 
-public class Wallet {
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "wallet")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Wallet extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "balance")
+    private Double balance;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private OwayUser userId;
+
 }
