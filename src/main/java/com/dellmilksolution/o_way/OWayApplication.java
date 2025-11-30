2258b636bac588d82f62fd1f70097a64f318b876
cvnxsolution <cvnxsolution@gmail.com>
2025-11-30 06:38:13 +0630

    initial commit

---
+++ src/main/java/com/dellmilksolution/o_way/OWayApplication.java
@@ -0,0 +1,13 @@
+package com.dellmilksolution.o_way;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OWayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dellmilksolution/o_way/OWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dellmilksolution.o_way;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
